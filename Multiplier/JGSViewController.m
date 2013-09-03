e2ce19a065f8563c46ce719c9b44d127c81f43d8
Jessica Smith <jessicasmith@client-75-102-117-157.mobility-up.psu.edu>
2013-09-03 17:19:07 -0400

    Have basic logic for incrementing through turns

--- Multiplier/JGSViewController.m
+++ Multiplier/JGSViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "JGSViewController.h"
+#define kNumTurns 10
 
 @interface JGSViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
@@ -17,10 +18,16 @@
 @property (weak, nonatomic) IBOutlet UIView *multiplicationBar;
 @property (weak, nonatomic) IBOutlet UILabel *resultLabel;
 @property (weak, nonatomic) IBOutlet UILabel *initialTitleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *answerCorrectnessLabel;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *answerSelectorBar;
-- (IBAction)answerSelector:(id)sender;
+- (IBAction)answerSelected:(id)sender;
 @property (weak, nonatomic) IBOutlet UIButton *startButtonLabel;
-- (IBAction)startButton:(id)sender;
+- (IBAction)startButtonClicked:(id)sender;
+
+@property BOOL gameStarted;                 // flag for if game has started or not
+@property NSInteger numTurns;               // number of turns that have been completed
+@property NSInteger numCorrectAnswers;      // number of correctly answered questions
+
 
 @end
 
@@ -31,8 +38,21 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
+    // Hide all elements initially except the start button and initial title for application
+    self.titleLabel.hidden = YES;
+    self.progressBar.hidden = YES;
+    self.firstMultiplierLabel.hidden = YES;
+    self.secondMultiplierLabel.hidden = YES;
+    self.multiplicationSignLabel.hidden = YES;
+    self.multiplicationBar.hidden = YES;
+    self.resultLabel.hidden = YES;
+    self.answerCorrectnessLabel.hidden = YES;
+    self.answerSelectorBar.hidden = YES;
     
-    
+    // Set initial variables for game
+    self.gameStarted = NO;
+    self.numTurns = 0;
+    self.numCorrectAnswers = 0;
 }
 
 - (void)didReceiveMemoryWarning
@@ -40,9 +60,50 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)answerSelected:(id)sender {
+}
 
-- (IBAction)answerSelector:(id)sender {
+-(void)GenerateNumbers {
+
+    
 }
-- (IBAction)startButton:(id)sender {
+
+- (IBAction)startButtonClicked:(id)sender {
+    
+    // Check if game has been started in order to properly display items on the page
+    if(!self.gameStarted) {
+        self.gameStarted = YES;
+        
+        // hide title that is in the middle of the screen
+        self.initialTitleLabel.hidden = YES;
+        
+        // unhide all elements except correctness label
+        self.titleLabel.hidden = NO;
+        self.progressBar.hidden = NO;
+        self.firstMultiplierLabel.hidden = NO;
+        self.secondMultiplierLabel.hidden = NO;
+        self.multiplicationSignLabel.hidden = NO;
+        self.multiplicationBar.hidden = NO;
+        self.resultLabel.hidden = NO;
+        self.answerSelectorBar.hidden = NO;
+    
+        // Change text on start button to be "Next"
+        [self.startButtonLabel setTitle:@"Next" forState:UIControlStateNormal];
+        
+    } else if(self.numTurns == kNumTurns-1) {
+        
+        // Change text on start button to be "Reset"
+        [self.startButtonLabel setTitle:@"Reset" forState:UIControlStateNormal];
+        
+        // Reset game state
+        self.gameStarted = NO;
+        self.numTurns = 0;
+        self.numCorrectAnswers = 0;
+        
+    } else {
+        // Increment the number of turns that has been completed
+        self.numTurns++;
+    }
 }
+
 @end
