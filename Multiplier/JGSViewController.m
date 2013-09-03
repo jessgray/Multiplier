90fb815f36160f8cd23360610cf61735871b4d9c
Jessica Smith <jessicasmith@client-75-102-117-157.mobility-up.psu.edu>
2013-09-03 16:23:43 -0400

    Finished setting up outlets

--- Multiplier/JGSViewController.m
+++ Multiplier/JGSViewController.m
@@ -9,6 +9,18 @@
 #import "JGSViewController.h"
 
 @interface JGSViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UIProgressView *progressBar;
+@property (weak, nonatomic) IBOutlet UILabel *firstMultiplierLabel;
+@property (weak, nonatomic) IBOutlet UILabel *secondMultiplierLabel;
+@property (weak, nonatomic) IBOutlet UILabel *multiplicationSignLabel;
+@property (weak, nonatomic) IBOutlet UIView *multiplicationBar;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+@property (weak, nonatomic) IBOutlet UILabel *initialTitleLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *answerSelectorBar;
+- (IBAction)answerSelector:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *startButtonLabel;
+- (IBAction)startButton:(id)sender;
 
 @end
 
@@ -18,6 +30,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +41,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)answerSelector:(id)sender {
+}
+- (IBAction)startButton:(id)sender {
+}
 @end
